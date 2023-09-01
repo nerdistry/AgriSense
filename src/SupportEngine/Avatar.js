d32767a5a37dd122990bf5b7ef202ab0b3014afc
Arabella Fanisheba <arabella.mutende@strathmore.edu>
2023-09-02 00:47:05 +0300

    Cool Avatar effect.

---
+++ src/SupportEngine/Avatar.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+
+import { styles } from "./styles";
+
+const Avatar = props => {
+  const [hovered, setHovered] = useState(false);
+
+  return (
+    <div style={props.style}>
+      <div
+      className='transition-3'
+      style={{
+        ...styles.avatarHello,
+        ...{opacity: hovered ? '1' : '0'}
+      }}>
+        Hey, it's Bella!
+        </div>
+      <div
+      className='transition-3'
+        onMouseEnter={() => setHovered(true)}
+        onMouseLeave={() => setHovered(false)}
+        style={{ ...styles.chatWithMeButton, ...{ border:hovered ? '1px solid #f9f0ff' : '4px solid #7a39e0'}}}
+      />
+    </div>
+  );
+};
+
+export default Avatar;
--- src/SupportEngine/index.js
+++ src/SupportEngine/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 
+import Avatar from "./Avatar";
 
 const SupportEngine = () => {
-    return (
-        <div>Support Engine</div>
-    )
+  return (
+    <div>
+      <Avatar
+      style={{ position: "fixed", bottom: "24px", right: "24px"  }}
+       />
+    </div>
+  )
 }
 
 export default SupportEngine;
-
